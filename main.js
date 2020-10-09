636b23df39c0bd892430841b0e953f9891ad772e
jbernago <jbernago@everis.com>
2020-10-09 14:15:14 +0200

    Resolución problemas práctiva JavaScript

--- main.js
+++ main.js
@@ -1,145 +1,154 @@
-let projects = [
-    {
-        title: 'SKM: intranet inteligente1',
-        description: 'Proyecto interno',
-        labels: ['Intranet', 'Buscador inteligente'],
-        type: 'enCurso'
-    },
-    {
-        title: 'SKM: intranet inteligente2',
-        description: 'Proyecto interno',
-        labels: ['Intranet', 'Buscador inteligente'],
-        type: 'relacionados'
-    },
-    {
-        title: 'SKM: intranet inteligente3',
-        description: 'Proyecto interno',
-        labels: ['Intranet', 'Buscador inteligente'],
-        type: 'cerrados'
-    },
-    {
-        title: 'SKM: intranet inteligente4',
-        description: 'Proyecto interno',
-        labels: ['Intranet', 'Buscador inteligente'],
-        type: 'cerrados'
-    },
-    {
-        title: 'SKM: intranet inteligente5',
-        description: 'Proyecto interno',
-        labels: ['Intranet', 'Buscador inteligente'],
-        type: 'enCurso'
-    }
-];
+window.onload = function() {
+    loader();
+};
 
-    // POR DEFECTO
-    filterList("");
 
-    // ALL BUTTON
-    let allButton = document.getElementById('allButton');
-    allButton.addEventListener('click', ()=>filterList(""));
+function loader(){
+
+    let projects = [
+        {
+            title: 'SKM: intranet inteligente1',
+            description: 'Proyecto interno',
+            labels: ['Intranet', 'Buscador inteligente'],
+            type: 'enCurso'
+        },
+        {
+            title: 'SKM: intranet inteligente2',
+            description: 'Proyecto interno',
+            labels: ['Intranet', 'Buscador inteligente'],
+            type: 'relacionados'
+        },
+        {
+            title: 'SKM: intranet inteligente3',
+            description: 'Proyecto interno',
+            labels: ['Intranet', 'Buscador inteligente'],
+            type: 'cerrados'
+        },
+        {
+            title: 'SKM: intranet inteligente4',
+            description: 'Proyecto interno',
+            labels: ['Intranet', 'Buscador inteligente'],
+            type: 'cerrados'
+        },
+        {
+            title: 'SKM: intranet inteligente5',
+            description: 'Proyecto interno',
+            labels: ['Intranet', 'Buscador inteligente'],
+            type: 'enCurso'
+        }
+    ];
     
+        // POR DEFECTO
+        filterList("");
+    
+        // ALL BUTTON
+        let allButton = document.querySelector('#allButton');
+        allButton.addEventListener('click', ()=>filterList(""));
+        
+    
+        // PROCESS BUTTON
+        let processButton = document.querySelector('#processButton');
+        processButton.addEventListener('click', ()=>filterList("enCurso"));
+        
+    
+    
+        // REL BUTTON
+        let relButton = document.querySelector('#relButton');
+        relButton.addEventListener('click', ()=>filterList("relacionados"));
+    
+    
+        // CLOSED BUTTON
+        let closedButton = document.querySelector('#closedButton');
+        closedButton.addEventListener('click', ()=>filterList("cerrados"));
+    
+    
+        function filterList(typeProject){
 
-    // PROCESS BUTTON
-    let processButton = document.getElementById('processButton');
-    processButton.addEventListener('click', ()=>filterList("enCurso"));
+            let allList = document.querySelector('#allList');
+            let processList = document.querySelector('#processList');
+            let relList = document.querySelector('#relList');
+            let closedList = document.querySelector('#closedList');
 
+            if(typeProject === "enCurso"){
+                processList.classList.add('card__tab-menu--active');
 
-    // REL BUTTON
-    let relButton = document.getElementById('relButton');
-    relButton.addEventListener('click', ()=>filterList("relacionados"));
+                allList.classList.remove('card__tab-menu--active');
 
+                relList.classList.remove('card__tab-menu--active');
 
-    // CLOSED BUTTON
-    let closedButton = document.getElementById('closedButton');
-    closedButton.addEventListener('click', ()=>filterList("cerrados"));
+                closedList.classList.remove('card__tab-menu--active');
+            }
+            else if(typeProject === "relacionados"){
+                relList.classList.add('card__tab-menu--active');
 
+                allList.classList.remove('card__tab-menu--active');
 
-    function filterList(typeProject){
+                processList.classList.remove('card__tab-menu--active');
 
+                closedList.classList.remove('card__tab-menu--active');
+            }
+            else if(typeProject === "cerrados"){
+                closedList.classList.add('card__tab-menu--active');
 
-        document.getElementById("card__project-wrapper").innerHTML= "";
+                allList.classList.remove('card__tab-menu--active');
 
-        if(typeProject != ""){
-            let projectProcess = projects.filter((projects) =>{
-                return projects.type === typeProject;
-            });
+                processList.classList.remove('card__tab-menu--active');
 
+                relList.classList.remove('card__tab-menu--active');
+            }
+            else{
+                allList.classList.add('card__tab-menu--active');
+
+                processList.classList.remove('card__tab-menu--active');
+
+                relList.classList.remove('card__tab-menu--active');
+
+                closedList.classList.remove('card__tab-menu--active');
+            }
+
+    
+    
+            document.querySelector("#card__project-wrapper").innerHTML= "";
+            let projectProcess;
+            if(typeProject !== ""){
+                projectProcess = projects.filter((projects) =>{
+                    return projects.type === typeProject;
+                });
+            }
+            else{
+                projectProcess = projects;
+            }
             projectProcess.forEach((a) => {
 
-                let card__project = document.createElement("div");
-                card__project.setAttribute("class", "card__project");
+                let cardProject = document.createElement("div");
+                cardProject.classList.add("card__project");
 
                     // CARD__LINK
-                    let card__link = document.createElement("a");
-                    let linkText =  document.createTextNode(a.title);
-                    card__link.appendChild(linkText);
-                    card__link.setAttribute("href", "#");
-                    card__link.setAttribute("class", "card__link");
-                    card__project.appendChild(card__link);
+                    let cardLink = document.createElement("a");
+                    cardLink.textContent = a.title;
+                    cardLink.setAttribute("href", "#");
+                    cardLink.classList.add("card__link");
+                    cardProject.appendChild(cardLink);
 
                     // CARD__PARAGRAPH
-                    let card__paragraph = document.createElement("span");
-                    let spanText =  document.createTextNode(a.description);
-                    card__paragraph.appendChild(spanText);
-                    card__paragraph.setAttribute("class", "card__paragraph");
-                    card__project.appendChild(card__paragraph);
+                    let cardParagraph = document.createElement("span");
+                    cardParagraph.textContent = a.description
+                    cardParagraph.classList.add("card__paragraph");
+                    cardProject.appendChild(cardParagraph);
 
                     // CARD__PILLS
-                    let card__projectPills = document.createElement("div");
-                    card__projectPills.setAttribute("class", "card__project-pills");
+                    let cardProjectPills = document.createElement("div");
+                    cardProjectPills.classList.add("card__project-pills");
                     a.labels.forEach((e) => {
-                        let card__pill = document.createElement("p");
-                        let pillText =  document.createTextNode(e);
-                        card__pill.appendChild(pillText);
-                        card__pill.setAttribute("class", "card__pill ellipsis");
-
-                        card__projectPills.appendChild(card__pill);
+                        let cardPill = document.createElement("span");
+                        cardPill.textContent = e;
+                        cardPill.classList.add("card__pill", "ellipsis");
+                        cardProjectPills.appendChild(cardPill);
                     });
-                    card__project.appendChild(card__projectPills);
+                    cardProject.appendChild(cardProjectPills);
 
-                document.getElementById("card__project-wrapper").appendChild(card__project);
+                document.querySelector("#card__project-wrapper").appendChild(cardProject);
             });
         }
-        else{
-            projects.forEach((a) => {
-
-            let card__project = document.createElement("div");
-            card__project.setAttribute("class", "card__project");
     
-                // CARD__LINK
-                let card__link = document.createElement("a");
-                let linkText =  document.createTextNode(a.title);
-                card__link.appendChild(linkText);
-                card__link.setAttribute("href", "#");
-                card__link.setAttribute("class", "card__link");
-                card__project.appendChild(card__link);
-    
-                // CARD__PARAGRAPH
-                let card__paragraph = document.createElement("span");
-                let spanText =  document.createTextNode(a.description);
-                card__paragraph.appendChild(spanText);
-                card__paragraph.setAttribute("class", "card__paragraph");
-                card__project.appendChild(card__paragraph);
-    
-                // CARD__PILLS
-                let card__projectPills = document.createElement("div");
-                card__projectPills.setAttribute("class", "card__project-pills");
-                a.labels.forEach((e) => {
-                    let card__pill = document.createElement("p");
-                    let pillText =  document.createTextNode(e);
-                    card__pill.appendChild(pillText);
-                    card__pill.setAttribute("class", "card__pill ellipsis");
-    
-                    card__projectPills.appendChild(card__pill);
-                });
-                card__project.appendChild(card__projectPills);
-    
-    
-                document.getElementById("card__project-wrapper").appendChild(card__project);
-            });
-        }
-
-
-    }
-
-
+}
